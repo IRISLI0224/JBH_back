b6beec1351479ca38eae24527fe69a1282f7a7c0
jinzhih <jessie.houjinzhi@gmail.com>
2021-06-14 02:23:58 +1000

    test succeed

---
+++ src/controllers/user.js
@@ -0,0 +1,14 @@
+const User = require('../models/user');
+
+async function addUser(req, res) {
+    const {
+        firstName,
+        lastName,
+    } = req.body;
+    const user = new User({ firstName, lastName });
+    await user.save();
+    return res.json({ firstName, lastName });
+}
+
+
+module.exports = { addUser };
