1f380498178499dbbbe1b8131f812000be705037
Tianyi <thua0026@student.monash.edu>
2021-07-13 21:57:44 +1000

    remove console.log

--- src/controllers/payment.js
+++ src/controllers/payment.js
@@ -2,9 +2,8 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_TEST);
 
 const createPayment = async (req, res) => {
-  console.log('stripe-routes.js 9 | route reached', req.body);
   const { amount, id, receipt_email } = req.body;
-  console.log('stripe-routes.js 10 | amount and id', amount, id);
+
   try {
     const payment = await stripe.paymentIntents.create({
       amount,
@@ -14,13 +13,12 @@ const createPayment = async (req, res) => {
       confirm: true,
       receipt_email,
     });
-    console.log('stripe-routes.js 19 | payment', payment);
+
     res.json({
       message: 'Payment Successful',
       success: true,
     });
   } catch (error) {
-    console.log('stripe-routes.js 17 | error', error);
     res.json({
       message: 'Payment Failed',
       success: false,
