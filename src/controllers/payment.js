3869d267f55fd5c624477b495d70424d7ef09618
Tianyi <thua0026@student.monash.edu>
2021-07-25 22:48:26 +1000

    update route for payment

--- src/controllers/payment.js
+++ src/controllers/payment.js
@@ -2,26 +2,31 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_TEST);
 
 // eslint-disable-next-line camelcase
 const createPayment = async (amount, id, receipt_email) => {
-  try {
-    const payment = await stripe.paymentIntents.create({
-      amount,
-      currency: 'AUD',
-      description: 'Japanese Bathhouse',
-      payment_method: id,
-      confirm: true,
-      receipt_email,
-    });
+  const payment = await stripe.paymentIntents.create({
+    amount,
+    currency: 'AUD',
+    description: 'Japanese Bathhouse',
+    payment_method: id,
+    confirm: true,
+    receipt_email,
+  });
+};
 
-    return {
+const createPaymentContainer = async (req, res) => {
+  // eslint-disable-next-line camelcase
+  const { amount, id, receipt_email } = req.body;
+  try {
+    const payment = await createPayment(amount, id, receipt_email);
+    return res.json({
       message: 'Payment Successful',
       success: true,
-    };
+    });
   } catch (error) {
-    return {
+    return res.json({
       message: 'Payment Failed',
       success: false,
-    };
+    });
   }
 };
 
-module.exports = { createPayment };
+module.exports = { createPayment, createPaymentContainer };
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ const express = require('express');
 const bookingRouter = require('./routes/booking');
 const sessionRouter = require('./routes/session');
 const loginRoute = require('./routes/login');
+const paymentRouter = require('./routes/payment');
 
 const router = express.Router();
 
 router.use('/login', loginRoute);
 router.use('/bookings', bookingRouter);
 router.use('/sessions', sessionRouter);
+router.use('/payment', paymentRouter);
 
 module.exports = router;
