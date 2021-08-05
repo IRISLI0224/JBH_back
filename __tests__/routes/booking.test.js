3f6b8195291cf7fa450da9a45b48f6f7852ac1ba
Tianyi <thua0026@student.monash.edu>
2021-08-05 23:49:59 +1000

    Update booking test case

--- __tests__/routes/booking.test.js
+++ __tests__/routes/booking.test.js
@@ -8,7 +8,7 @@ const { generateToken } = require('../../src/utils/jwt');
 
 const request = supertest(app);
 
-const TOKEN = generateToken({ id: 'fake_id' });
+const TOKEN = generateToken({ id: 'fake_id' }); // to pass the AuthGuard middleware.
 
 const today = new Date();
 
@@ -26,32 +26,23 @@ describe('/bookings', () => {
     await Session.deleteMany({}).exec();
   });
 
-  // afterEach(async () => {
-  //   await Booking.deleteMany({}).exec();
-  // });
+  afterEach(async () => {
+    await Booking.deleteMany({}).exec();
+    await Session.deleteMany({}).exec();
+  });
 
   // ======================= ADD BOOKING ===================
+
   describe('POST /', () => {
     let validateBooking;
-    beforeAll(async () => {
-      const sessionData = {
+    let sessionData;
+    beforeEach(async () => {
+      sessionData = {
         date: '2021-07-10',
         time: 0,
         maxNumber: 20,
       };
       await Session.insertMany(sessionData);
-
-      const { id } = await stripe.paymentMethods.create(
-        {
-          type: 'card',
-          card: {
-            number: '4242424242424242',
-            exp_month: today.getMonth() + 1,
-            exp_year: today.getFullYear() + 1,
-          },
-        },
-        { apiKey: process.env.STRIPE_SECRET_TEST }
-      );
 
       validateBooking = {
         bookingDate: sessionData.date,
@@ -63,15 +54,87 @@ describe('/bookings', () => {
         phone: '0123456789',
         dateOfBirth: '1968-01-02',
         paidAmount: '68',
-        id,
       };
     });
 
-    const addBooking = async (body) => request.post('/api/bookings').send(body);
+    const addBooking = async (body, isRealPaymentId) => {
+      let newBody = body;
+      // to pass the payment operation in addBooking.
+      if (isRealPaymentId) {
+        const { id } = await stripe.paymentMethods.create(
+          {
+            type: 'card',
+            card: {
+              number: '4242424242424242',
+              exp_month: today.getMonth() + 1,
+              exp_year: today.getFullYear() + 1,
+            },
+          },
+          { apiKey: process.env.STRIPE_SECRET_TEST },
+        );
+        newBody = { ...body, id };
+      }
+      return request.post('/api/bookings').send(newBody);
+    };
 
     it('should return 201 if request is valid', async () => {
-      const res = await addBooking(validateBooking);
+      const res = await addBooking(validateBooking, true);
       expect(res.statusCode).toBe(201);
+    });
+
+    it('should save booking if request is valid', async () => {
+      const res = await addBooking(validateBooking, true);
+      const booking = await Booking.findOne({ email: 'Nick@js.com' }).exec();
+      expect(booking.bookingDate.toISOString().slice(0, 10)).toBe(
+        sessionData.date,
+      );
+      expect(booking.numOfGuests.toString()).toBe('10');
+      expect(booking.firstName).toBe('POST');
+      expect(booking.lastName).toBe('Kate');
+      expect(booking.gender.toString()).toBe('true');
+      expect(booking.phone).toBe('0123456789');
+      expect(booking.dateOfBirth.toISOString().slice(0, 10)).toBe('1968-01-02');
+      expect(booking.paidAmount.toString()).toBe('68');
+    });
+
+    it.each`
+      field            | value
+      ${'firstName'}   | ${''}
+      ${'firstName'}   | ${500}
+      ${'firstName'}   | ${'F'}
+      ${'firstName'}   | ${'asdfghjklzxcvbnmqwert'}
+      ${'lastName'}    | ${''}
+      ${'lastName'}    | ${500}
+      ${'lastName'}    | ${'F'}
+      ${'lastName'}    | ${'asdfghjklzxcvbnmqwert'}
+      ${'gender'}      | ${''}
+      ${'gender'}      | ${'adbded'}
+      ${'email'}       | ${''}
+      ${'email'}       | ${'abdc@adv.au'}
+      ${'email'}       | ${'abdc@au'}
+      ${'phone'}       | ${''}
+      ${'phone'}       | ${'abdcddef'}
+      ${'phone'}       | ${'1234567'}
+      ${'phone'}       | ${'12345678901'}
+      ${'bookingDate'} | ${''}
+      ${'bookingDate'} | ${'abdcdefg'}
+      ${'bookingDate'} | ${'08 Aug 2021'}
+      ${'numOfGuests'} | ${''}
+      ${'numOfGuests'} | ${'abdcdefg'}
+      ${'numOfGuests'} | ${'-1'}
+      ${'numOfGuests'} | ${'1.25'}
+      ${'numOfGuests'} | ${'21'}
+      ${'dateOfBirth'} | ${''}
+      ${'dateOfBirth'} | ${'abdcdefg'}
+      ${'dateOfBirth'} | ${'08 Aug 2021'}
+      ${'paidAmount'}  | ${''}
+      ${'paidAmount'}  | ${'abdcdefg'}
+      ${'paidAmount'}  | ${'9'}
+      ${'paidAmount'}  | ${'9.25'}
+    `('should return 400 when $field is $value', async ({ field, value }) => {
+      validateBooking = { ...validateBooking, [field]: value };
+      const res = await addBooking(validateBooking, false);
+      expect(res.statusCode).toBe(400);
     });
   });
 
@@ -141,37 +204,43 @@ describe('/bookings', () => {
       ];
 
       await Booking.insertMany(validateBooking);
-      // 上述的booking添加到对应的session中.（addbooking中的步骤的mongoose版）
-      const session = await Session.findOneAndUpdate(
+      // 上述的booking(第三项)添加到对应的session中用于测试.
+      const session1 = await Session.findOneAndUpdate(
         { date: validateBooking[2].bookingDate },
         { $push: { bookings: validateBooking[2].numOfGuests } },
-        { new: true }
+        { new: true },
       );
     });
 
-    const getAllBookings = async () =>
-      request.get('/api/bookings/all').set('Authorization', `Bearer ${TOKEN}`);
+    const getAllBookings = async () => request.get('/api/bookings/all').set('Authorization', `Bearer ${TOKEN}`);
 
     it('should return 200 if request is valid', async () => {
       const res = await getAllBookings();
       expect(res.statusCode).toBe(200);
+    });
+
+    it('should return bookings if request is valid', async () => {
+      const res = await getAllBookings();
       expect(res.body.length).toEqual(3);
-      expect(res.body[0]).toHaveProperty('sessions');
-      // 完善expect的判断信息
+      expect(res.body[0]).toHaveProperty('bookingNum');
+      expect(res.body[0].bookingNum).toBe('DBJ731345458');
+      expect(res.body[1]).toHaveProperty('bookingNum');
+      expect(res.body[1].bookingNum).toBe('DBJ123456789');
+      expect(res.body[2]).toHaveProperty('bookingNum');
+      expect(res.body[2].bookingNum).toBe('DBJ987654321');
     });
 
     // ------------------getBookingsByMonth-------------------
-    const getBookingsByMonth = async (year, month) =>
-      request.get(`/api/bookings/monthly/${year}/${month}`);
+    const getBookingsByMonth = async (year, month) => request.get(`/api/bookings/monthly/${year}/${month}`);
 
     it('should return 201 if request is valid', async () => {
       const dateDetails = sessionData[2].date.split('-');
-      const year = dateDetails[0];
-      const month = dateDetails[1];
-      const day = dateDetails[2];
-      // console.log(day);
+      const [year, month, day] = [
+        dateDetails[0],
+        dateDetails[1],
+        dateDetails[2],
+      ];
       const res = await getBookingsByMonth(year, month);
-      // console.log(res.body);
       expect(res.statusCode).toBe(200);
       expect(res.body).toHaveProperty('date');
       expect(res.body.date).toBe(`${year}-${month}`);
@@ -181,10 +250,9 @@ describe('/bookings', () => {
 
     // --------------------getBookingsByArgs---------------
 
-    const getBookingsByArgs = async (field, value) =>
-      request
-        .get(`/api/bookings/${field}/${value}`)
-        .set('Authorization', `Bearer ${TOKEN}`);
+    const getBookingsByArgs = async (field, value) => request
+      .get(`/api/bookings/${field}/${value}`)
+      .set('Authorization', `Bearer ${TOKEN}`);
 
     it.each`
       param            | field            | value
@@ -199,26 +267,36 @@ describe('/bookings', () => {
         let requestValue = value;
         if (param === 'id') {
           requestValue = (await Booking.findOne({}).exec())._id.toString();
-          // console.log(requestValue);
         }
         const res = await getBookingsByArgs(param, requestValue);
         expect(res.statusCode).toBe(200);
-        // console.log(res.body);
-        const expectedValue =
-          param === 'bookingdate'
-            ? `${requestValue}T00:00:00.000Z`
-            : requestValue;
+        const expectedValue = param === 'bookingdate'
+          ? `${requestValue}T00:00:00.000Z`
+          : requestValue;
         expect(res.body[0][field]).toEqual(expectedValue);
-      }
+      },
     );
+
+    it('should return 404 if the pathname is invalid', async () => {
+      const res = await getBookingsByArgs('pho', '0123456789');
+      expect(res.statusCode).toBe(404);
+    });
   });
 
   // ======================= UPDATE BOOKING ===================
+
   describe('UPDATE /', () => {
     let validateBooking;
     beforeEach(async () => {
+      const sessionData = {
+        date: '2021-07-10',
+        time: 0,
+        maxNumber: 20,
+      };
+      await Session.insertMany(sessionData);
+
       validateBooking = {
-        bookingDate: '2021-07-10',
+        bookingDate: sessionData.date,
         numOfGuests: '10',
         firstName: 'Nick',
         lastName: 'Kate',
@@ -231,7 +309,12 @@ describe('/bookings', () => {
         id: '13adfaew',
       };
       await Booking.insertMany(validateBooking);
-      // console.log(await Booking.find({}));
+      // 上述的booking添加到对应的session中.
+      const session = await Session.findOneAndUpdate(
+        { date: validateBooking.bookingDate },
+        { $push: { bookings: validateBooking.numOfGuests } },
+        { new: true },
+      );
     });
 
     const updateBookingsByArgs = async (field, fValue, path, pValue) => {
@@ -254,23 +337,25 @@ describe('/bookings', () => {
       ['paidAmount', '70', '70'],
       ['bookingNum', 'DBJ731345459', 'DBJ731345459'],
     ];
+
     // --------------------updateBookingByBookingNum---------------
+
     it.each(cases)(
       'should return 201 when %s is %s',
       async (field, fValue, eValue) => {
-        // console.log({ field, fValue });
         const res = await updateBookingsByArgs(
           field,
           fValue,
           'bookingnum',
-          validateBooking.bookingNum
+          validateBooking.bookingNum,
         );
-        // console.log(res.body);
         expect(res.statusCode).toBe(201);
         expect(res.body[field].toString()).toBe(eValue);
-      }
+      },
     );
+
     // --------------------updateBookingById---------------
+
     it.each(cases)(
       'should return 201 when %s is %s',
       async (field, fValue, eValue) => {
@@ -278,11 +363,32 @@ describe('/bookings', () => {
         const res = await updateBookingsByArgs(field, fValue, 'id', bookingId);
         expect(res.statusCode).toBe(201);
         expect(res.body[field].toString()).toBe(eValue);
-      }
+      },
     );
+
+    it('should return 404 if the first pathname is invalid', async () => {
+      const res = await updateBookingsByArgs(
+        'bookingDate',
+        '2021-07-11',
+        'boo',
+        validateBooking.bookingNum,
+      );
+      expect(res.statusCode).toBe(404);
+    });
+
+    it('should return 200 if the second pathname is invalid', async () => {
+      const res = await updateBookingsByArgs(
+        'bookingDate',
+        '2021-07-11',
+        'bookingnum',
+        'adfdas12354',
+      );
+      expect(res.statusCode).toBe(200);
+    });
   });
 
   // ======================= DELETE BOOKING ===================
+
   describe('DELETE /', () => {
     let validateBooking;
     beforeEach(async () => {
@@ -307,21 +413,29 @@ describe('/bookings', () => {
         id: '13adfaew',
       };
       await Booking.insertMany(validateBooking);
-      // 上述的booking添加到对应的session中.（addbooking中的步骤的mongoose版）
+      // 上述的booking添加到对应的session中.
       const session = await Session.findOneAndUpdate(
         { date: validateBooking.bookingDate },
         { $push: { bookings: validateBooking.numOfGuests } },
-        { new: true }
+        { new: true },
       );
     });
+
     //  -----------------deleteBookingById --------------------
+
+    const deleteBooking = async (param) => request
+      .delete(`/api/bookings/id/${param}`)
+      .set('Authorization', `Bearer ${TOKEN}`);
 
     it('should return 200 if delete is successful', async () => {
       const bookingId = (await Booking.findOne({}).exec())._id;
-      const res = await request
-        .delete(`/api/bookings/id/${bookingId}`)
-        .set('Authorization', `Bearer ${TOKEN}`);
+      const res = await deleteBooking(bookingId);
       expect(res.statusCode).toBe(200);
+    });
+
+    it('should return 500 if the bookingId is invalid', async () => {
+      const res = await deleteBooking('testId001');
+      expect(res.statusCode).toBe(500);
     });
   });
 });
--- src/controllers/booking.js
+++ src/controllers/booking.js
@@ -1,7 +1,7 @@
-const Booking = require("../models/booking");
-const Session = require("../models/session");
-const { createPayment } = require("./payment");
-const { genBookingNum } = require("../utils/gen");
+const Booking = require('../models/booking');
+const Session = require('../models/session');
+const { createPayment } = require('./payment');
+const { genBookingNum } = require('../utils/gen');
 
 // 添加booking并生成用户密码
 const addBooking = async (req, res) => {
@@ -20,11 +20,11 @@ const addBooking = async (req, res) => {
   const { id } = req.body;
   const paymentRes = {
     success: {
-      message: "Payment Successful",
+      message: 'Payment Successful',
       success: true,
     },
     fail: {
-      message: "Payment Failed",
+      message: 'Payment Failed',
       success: false,
     },
   };
@@ -32,7 +32,7 @@ const addBooking = async (req, res) => {
     const response = await createPayment(paidAmount * 100, id, email);
   } catch (error) {
     // stop addbooking request if the payment is failed.
-    return res.json(paymentRes.fail);
+    return res.status(error.statusCode).json(error.message);
   }
   // add booking if the payment is successful.
   const bookingNum = genBookingNum();
@@ -80,12 +80,12 @@ const getBookingsByMonth = async (req, res) => {
     bookingsExistenceArr.push(false);
     for (let j = 0; j < requestingSessions.length; j += 1) {
       const requestingDay = parseInt(
-        requestingSessions[j].date.split("-")[2],
-        10
+        requestingSessions[j].date.split('-')[2],
+        10,
       );
       if (
-        requestingDay === i + 1 &&
-        requestingSessions[j].bookings.length > 0
+        requestingDay === i + 1
+        && requestingSessions[j].bookings.length > 0
       ) {
         bookingsExistenceArr[i] = true;
         break;
@@ -104,11 +104,11 @@ const getBookingsByArgs = (args) => async (req, res) => {
   }
   return res.json(bookings);
 };
-const getBookingsByPhone = getBookingsByArgs("phone");
-const getBookingsByEmail = getBookingsByArgs("email");
-const getBookingByBookingNum = getBookingsByArgs("bookingNum");
-const getBookingsByBookingDate = getBookingsByArgs("bookingDate");
-const getBookingById = getBookingsByArgs("_id");
+const getBookingsByPhone = getBookingsByArgs('phone');
+const getBookingsByEmail = getBookingsByArgs('email');
+const getBookingByBookingNum = getBookingsByArgs('bookingNum');
+const getBookingsByBookingDate = getBookingsByArgs('bookingDate');
+const getBookingById = getBookingsByArgs('_id');
 
 // 根据bookingNum或者_id更新booking（client和admin登录后均有权限）
 // 改参加人数后session人数变动的逻辑还没写
@@ -118,17 +118,17 @@ const updateBookingByArgs = (args) => async (req, res) => {
   const newBooking = await Booking.findOneAndUpdate(
     { [args]: req.params[args] },
     { ...newBookingInfo },
-    { new: true }
+    { new: true },
   ).exec();
   if (!newBooking) return res.send(`找不到这个${args}的booking信息`);
   await newBooking.hashPassword();
   await newBooking.save();
   // 更新booking时更新关联到session的booking人数
   if (oldBooking.numOfGuests !== newBooking.numOfGuests) {
-    const date = oldBooking.bookingDate.toISOString().split("T")[0];
+    const date = oldBooking.bookingDate.toISOString().split('T')[0];
     const session = await Session.findOne({ date }).exec();
     const index = session.bookings.findIndex(
-      (numOfGuest) => numOfGuest === oldBooking.numOfGuests
+      (numOfGuest) => numOfGuest === oldBooking.numOfGuests,
     );
     session.bookings.splice(index, 1);
     session.bookings.push(newBooking.numOfGuests);
@@ -137,25 +137,25 @@ const updateBookingByArgs = (args) => async (req, res) => {
 
   return res.status(201).json(newBooking);
 };
-const updateBookingByBookingNum = updateBookingByArgs("bookingNum");
-const updateBookingById = updateBookingByArgs("_id");
+const updateBookingByBookingNum = updateBookingByArgs('bookingNum');
+const updateBookingById = updateBookingByArgs('_id');
 
 // 根据_id删除booking（仅admin登录后有权限）
 const deleteBookingById = async (req, res) => {
   const booking = await Booking.findByIdAndDelete(req.params._id).exec();
   if (!booking) {
-    return res.status(404).json("booking not found in this id");
+    return res.status(404).json('booking not found in this id');
   }
   // 删除booking时删除关联到session的booking人数
-  const date = booking.bookingDate.toISOString().split("T")[0];
+  const date = booking.bookingDate.toISOString().split('T')[0];
   const session = await Session.findOne({ date }).exec();
   const index = session.bookings.findIndex(
-    (numOfGuest) => numOfGuest === booking.numOfGuests
+    (numOfGuest) => numOfGuest === booking.numOfGuests,
   );
   session.bookings.splice(index, 1);
   await session.save();
 
-  return res.status(200).send("delete successful");
+  return res.status(200).send('delete successful');
 };
 
 module.exports = {
